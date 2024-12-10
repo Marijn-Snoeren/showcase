691f1114b7d3f2b432ea15e5561918b477c0b585
Marijn Snoeren <m.snoeren@student.fontys.nl>
2024-12-10 10:11:56 +0100

    nieuwe opzet

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,15 @@
 'use client';
 
-import { useEffect } from 'react';
 import React from 'react';
-import Lenis from 'lenis';
-import Block from '../components/Block';
-import Block2 from '../components/Block2';
+import Project1 from '../projects/Project1';
+import Project2 from '../projects/Project2';
+
 
 export default function Home() {
-
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time: DOMHighResTimeStamp) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
     <div className="min-h-screen flex flex-col text-black bg-white">
-      <header className="sticky top-0 z-20 flex justify-between px-6 py-4 bg-opacity-0 text-sm font-semibold uppercase -mt-14">
-        <span>JIRAM</span>
-        <span className="text-white">About</span>
-      </header>
-
-      <main className="space-y-0">
-        <Block
-          imageUrl="https://images.unsplash.com/photo-1722580629887-86861f85734e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          title="PROJECT"
-          number="01"
-        />
-        <Block2
-          imageUrl="https://images.unsplash.com/photo-1720048171180-a8178a198fa8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
-          title="PROJECT"
-          number="02"
-        />
-        <Block
-          imageUrl="https://images.unsplash.com/photo-1731329396266-8250ea4d56c1?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
-          title="PROJECT"
-          number="03"
-        />
-                <Block2
-          imageUrl="https://images.unsplash.com/photo-1720048171180-a8178a198fa8?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8"
-          title="PROJECT"
-          number="02"
-        />
-      </main>
+        <Project1/>
+        <Project2/>
     </div>
   );
 }
---
+++ projects/Project2.js
@@ -0,0 +1,30 @@
+'use client';
+
+export default function Project1() {
+  return (
+    <div className="flex h-screen snap-start">
+      <div className="flex-1 relative overflow-hidden">
+        <img
+          src="https://images.unsplash.com/photo-1722580629887-86861f85734e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Full Height Image"
+         className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+      </div>
+
+      <div className="bg-white flex-1 flex flex-col justify-between py-4 z-10">
+        <div className="flex-1 flex items-center justify-center">
+          <img
+            src="https://images.unsplash.com/photo-1722580629887-86861f85734e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Small Centered Image"
+            className="w-1/3 aspect-9/14 object-cover"
+          />
+        </div>
+
+        <div className="flex justify-between text-2xl font-semibold uppercase px-8">
+          <span>Project</span>
+          <span>01</span>
+        </div>
+      </div>
+    </div>
+  );
+}
